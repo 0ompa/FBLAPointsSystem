3f9dd4ec46c218e2d8ac0fe66e58e13db3abe957
0ompa <101219929+0ompa@users.noreply.github.com>
2023-09-18 10:34:10 -0400

    Add files via upload

---
+++ script.py
@@ -0,0 +1,47 @@
+# ---------------------------------------------------
+# Courtesy of Om Patil- AHS FBLA Secretary 2023-2024
+# FBLA Points System Automated in Python
+# ---------------------------------------------------
+
+# INSTRUCTIONS!!!
+# First, open the full member points system for the entire year
+# Paste all the First and Last Names into the First and Last name Task on Hand
+# Next, click on 'File' and hover over 'Download' and click on 'Comma Separated Values (.csv)'
+# Do this step for each of the grade levels
+# Open Visual Studio Code IDE
+# Open this script and all 4 csv files of each grade level
+# Run script for one csv file
+# After you run it, your data with the points should save in the csv file you downloaded. 
+# Follow these directions- https://blog.golayer.io/google-sheets/import-csv-to-google-sheets 
+# After you have done this, go ahead and copy-paste your data into the master google sheets.
+# Repeat the run process and transformation into the master sheets for all grade level csv's.
+
+
+import pandas as pd
+import string
+
+alphabet = list(string.ascii_lowercase)
+
+# CHANGE DIRECTORY FOR YOUR COMPUTER HERE- SWITCH CSV FILE FOR EACH GRADE LEVEL FOR EACH RUN
+df = pd.read_csv('9th.csv')
+
+num_of_attendees = 0
+task = df["First Name: Task on Hand:"].isnull()
+
+while task[num_of_attendees] != True:
+    num_of_attendees+=1
+
+total_members = len(df["Last Name"])
+
+
+
+for att in range(num_of_attendees):
+    for member in range(total_members):
+        if df.loc[att].at["Last Name: Task on Hand"].lower() == df.loc[member].at["Last Name"].lower():
+            if df.loc[att].at["First Name: Task on Hand:"].lower() == df.loc[member].at["First Name"].lower():
+                points = df.loc[member].at["Points"]
+                df.at[member, "Points"] = points + 1
+                #CHANGE CSV FILE NAME HERE TO GRADE LEVEL AS WELL
+                df.to_csv("9th.csv", index=False)
+
+print(df)
